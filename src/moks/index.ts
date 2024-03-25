8b8cb9587e942d2ba25bba5ea087672ff0f58dd2
Yuri Trondin <lopata@2ch.hk>
2024-03-25 22:58:43 +0500

    lesson 4

---
+++ src/moks/index.ts
@@ -0,0 +1,317 @@
+export const users = [
+    {
+        "_id": "6601b72a6118c3c68c7b1e6e",
+        "index": 0,
+        "guid": "0a640e83-ee30-417a-bff2-08a38e439d63",
+        "isActive": false,
+        "balance": "$2,068.91",
+        "picture": "http://placehold.it/32x32",
+        "age": 22,
+        "eyeColor": "blue",
+        "name": "Audra Lane",
+        "gender": "female",
+        "company": "PANZENT",
+        "email": "audralane@panzent.com",
+        "phone": "+1 (823) 517-2673",
+        "address": "890 Cornelia Street, Brady, Tennessee, 1758",
+        "about": "Nisi magna dolore consequat commodo magna aliquip deserunt ipsum nulla quis excepteur sunt. Pariatur nisi nostrud consectetur ex elit cillum pariatur ullamco do qui sunt in sunt aliqua. Esse adipisicing dolor dolore laborum enim. Id mollit voluptate non officia est ipsum est. Laborum ut eu veniam commodo ut anim.\r\n",
+        "registered": "2020-08-31T10:46:50 -05:00",
+        "latitude": -76.131787,
+        "longitude": -141.103521,
+        "tags": [
+            "ipsum",
+            "proident",
+            "non",
+            "proident",
+            "aliqua",
+            "minim",
+            "do"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Ward Buchanan"
+            },
+            {
+                "id": 1,
+                "name": "Vazquez Lyons"
+            },
+            {
+                "id": 2,
+                "name": "Jerri Ball"
+            }
+        ],
+        "greeting": "Hello, Audra Lane! You have 5 unread messages.",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "6601b72a9a518c8406efc2f2",
+        "index": 1,
+        "guid": "d6cb25bb-2d28-4210-97f5-f3a48a5a7f32",
+        "isActive": false,
+        "balance": "$1,525.32",
+        "picture": "http://placehold.it/32x32",
+        "age": 27,
+        "eyeColor": "brown",
+        "name": "Cecile Hayes",
+        "gender": "female",
+        "company": "XINWARE",
+        "email": "cecilehayes@xinware.com",
+        "phone": "+1 (802) 410-3386",
+        "address": "326 Cyrus Avenue, Salvo, Texas, 8714",
+        "about": "Adipisicing sit aliquip laboris Lorem aliqua dolore in qui culpa. Aute cillum ut cupidatat incididunt nulla. Ipsum anim proident ex ex nostrud ipsum mollit ut. Nisi id et magna aliquip id amet laborum excepteur in reprehenderit commodo cillum proident ut. Sunt incididunt consectetur irure deserunt voluptate voluptate in est ex nostrud consectetur eiusmod velit. Ipsum proident do et minim id officia. Velit do non laborum aliquip esse.\r\n",
+        "registered": "2018-11-03T10:11:12 -05:00",
+        "latitude": -33.879725,
+        "longitude": 131.179072,
+        "tags": [
+            "tempor",
+            "cupidatat",
+            "mollit",
+            "tempor",
+            "do",
+            "deserunt",
+            "commodo"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Amparo Berg"
+            },
+            {
+                "id": 1,
+                "name": "Marianne Becker"
+            },
+            {
+                "id": 2,
+                "name": "Rebekah Weber"
+            }
+        ],
+        "greeting": "Hello, Cecile Hayes! You have 8 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "6601b72a7e49acadd41c9ab6",
+        "index": 2,
+        "guid": "fe13fefc-ecf3-473a-8492-80a6830353b6",
+        "isActive": true,
+        "balance": "$3,390.34",
+        "picture": "http://placehold.it/32x32",
+        "age": 25,
+        "eyeColor": "blue",
+        "name": "Crawford Stark",
+        "gender": "male",
+        "company": "PAWNAGRA",
+        "email": "crawfordstark@pawnagra.com",
+        "phone": "+1 (855) 487-2185",
+        "address": "192 Vine Street, Osmond, Kansas, 5394",
+        "about": "Eiusmod incididunt eiusmod exercitation deserunt qui. Non dolor ipsum consequat quis in ullamco. Irure irure mollit nostrud non sit nisi velit do eiusmod consequat. Aute enim veniam anim sunt non sit. Reprehenderit dolore duis do cupidatat do ad excepteur.\r\n",
+        "registered": "2020-01-05T02:59:39 -05:00",
+        "latitude": 75.266584,
+        "longitude": 128.551311,
+        "tags": [
+            "exercitation",
+            "ullamco",
+            "dolore",
+            "consectetur",
+            "incididunt",
+            "reprehenderit",
+            "ullamco"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Carpenter Holder"
+            },
+            {
+                "id": 1,
+                "name": "Lou Myers"
+            },
+            {
+                "id": 2,
+                "name": "Donna Nolan"
+            }
+        ],
+        "greeting": "Hello, Crawford Stark! You have 6 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "6601b72a407a10501715592d",
+        "index": 3,
+        "guid": "5c9f326d-0e0d-4d7e-b78b-bf9f2c3757cb",
+        "isActive": false,
+        "balance": "$1,834.03",
+        "picture": "http://placehold.it/32x32",
+        "age": 33,
+        "eyeColor": "blue",
+        "name": "Keith Thompson",
+        "gender": "male",
+        "company": "ASSITIA",
+        "email": "keiththompson@assitia.com",
+        "phone": "+1 (939) 583-2196",
+        "address": "250 Grand Avenue, Ronco, Hawaii, 3919",
+        "about": "Culpa ut dolor tempor nostrud minim deserunt elit. Et aliqua mollit sit sunt irure quis quis aliqua adipisicing magna aliqua cupidatat. Dolore est esse duis eu culpa anim minim enim. Culpa occaecat excepteur aute occaecat consequat voluptate quis aliquip quis consequat ea pariatur. Deserunt proident consequat sunt esse minim id cillum.\r\n",
+        "registered": "2024-02-06T02:46:04 -05:00",
+        "latitude": 6.817004,
+        "longitude": 123.812943,
+        "tags": [
+            "quis",
+            "magna",
+            "pariatur",
+            "eiusmod",
+            "proident",
+            "anim",
+            "incididunt"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Shelton Espinoza"
+            },
+            {
+                "id": 1,
+                "name": "Dawn Eaton"
+            },
+            {
+                "id": 2,
+                "name": "Kerri Rich"
+            }
+        ],
+        "greeting": "Hello, Keith Thompson! You have 8 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "_id": "6601b72a14ba2001a1e94d38",
+        "index": 4,
+        "guid": "27d8f15b-dd5a-4f42-95bd-9ad13c583c50",
+        "isActive": false,
+        "balance": "$3,592.29",
+        "picture": "http://placehold.it/32x32",
+        "age": 26,
+        "eyeColor": "blue",
+        "name": "Williams Ashley",
+        "gender": "male",
+        "company": "GINKLE",
+        "email": "williamsashley@ginkle.com",
+        "phone": "+1 (899) 493-2606",
+        "address": "541 Waldane Court, Yogaville, Indiana, 734",
+        "about": "Ipsum veniam labore non voluptate consectetur laboris velit pariatur ea qui Lorem eu cillum commodo. Veniam et ad laboris ut aliqua velit aliqua. Est sit ea id dolor elit elit in. Qui nisi nostrud elit occaecat. Duis est cupidatat ea elit. Sint aliquip dolore quis ullamco qui officia tempor Lorem do sit non sint.\r\n",
+        "registered": "2014-05-27T11:13:43 -06:00",
+        "latitude": -65.213194,
+        "longitude": 51.521954,
+        "tags": [
+            "et",
+            "ad",
+            "aliquip",
+            "deserunt",
+            "veniam",
+            "aliquip",
+            "est"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Lane Macdonald"
+            },
+            {
+                "id": 1,
+                "name": "Stevenson Quinn"
+            },
+            {
+                "id": 2,
+                "name": "Bright Cash"
+            }
+        ],
+        "greeting": "Hello, Williams Ashley! You have 7 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "6601b72acfcc41bb8e5c5838",
+        "index": 5,
+        "guid": "c4437e13-9bf1-4e03-b2b6-8f2bf3423888",
+        "isActive": false,
+        "balance": "$1,994.14",
+        "picture": "http://placehold.it/32x32",
+        "age": 20,
+        "eyeColor": "blue",
+        "name": "Rosalind Butler",
+        "gender": "female",
+        "company": "COLLAIRE",
+        "email": "rosalindbutler@collaire.com",
+        "phone": "+1 (968) 543-3938",
+        "address": "695 Allen Avenue, Thomasville, North Carolina, 9379",
+        "about": "Nulla sit aliquip excepteur laborum Lorem occaecat. Tempor ullamco reprehenderit ad nostrud enim duis do aute id culpa labore fugiat amet. Officia esse ex excepteur mollit magna ut in incididunt id. Aliquip dolor eiusmod eu nostrud sit ea sunt velit officia Lorem do dolor consequat. Proident ipsum laboris aliqua exercitation reprehenderit cupidatat eu. Anim amet sit dolore occaecat aute ipsum do est. Fugiat anim fugiat laborum eiusmod laboris mollit.\r\n",
+        "registered": "2022-03-31T01:32:07 -05:00",
+        "latitude": -76.254937,
+        "longitude": 14.06011,
+        "tags": [
+            "velit",
+            "nostrud",
+            "proident",
+            "pariatur",
+            "enim",
+            "Lorem",
+            "qui"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Woodward Elliott"
+            },
+            {
+                "id": 1,
+                "name": "Harvey Stanton"
+            },
+            {
+                "id": 2,
+                "name": "Marilyn Potter"
+            }
+        ],
+        "greeting": "Hello, Rosalind Butler! You have 3 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "_id": "6601b72a594b3dce65343890",
+        "index": 6,
+        "guid": "3d907056-4737-4c4c-816a-0454f59e579d",
+        "isActive": true,
+        "balance": "$1,470.98",
+        "picture": "http://placehold.it/32x32",
+        "age": 24,
+        "eyeColor": "blue",
+        "name": "Jannie Craft",
+        "gender": "female",
+        "company": "GEEKY",
+        "email": "janniecraft@geeky.com",
+        "phone": "+1 (812) 584-2748",
+        "address": "599 Irving Avenue, Canby, Arkansas, 5882",
+        "about": "Veniam velit excepteur consectetur pariatur Lorem minim do aliqua adipisicing dolor fugiat exercitation. Ex commodo enim sit est veniam. Lorem sit in sit ut mollit ut aliqua.\r\n",
+        "registered": "2016-06-27T01:35:06 -05:00",
+        "latitude": -44.088512,
+        "longitude": 10.297149,
+        "tags": [
+            "quis",
+            "sint",
+            "exercitation",
+            "amet",
+            "sint",
+            "aliquip",
+            "officia"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Maude Hickman"
+            },
+            {
+                "id": 1,
+                "name": "Nichole Dillon"
+            },
+            {
+                "id": 2,
+                "name": "Parks Aguirre"
+            }
+        ],
+        "greeting": "Hello, Jannie Craft! You have 2 unread messages.",
+        "favoriteFruit": "apple"
+    }
+]
