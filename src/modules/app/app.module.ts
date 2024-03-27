630b67cffb64b3514dc4eeb8c5ffd7008d0eed8b
Yuri Trondin <lopata@2ch.hk>
2024-03-27 22:52:35 +0500

    lesson 7

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -2,15 +2,33 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from '../user/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configurations from '../../configurations'
+import { SequelizeModule } from '@nestjs/sequelize';
+
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
     load: [
       configurations
     ]
-  }), UsersModule],
+  }), UsersModule, SequelizeModule.forRootAsync({
+    imports: [
+      ConfigModule
+    ],
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => ({
+      dialect: 'postgres',
+      port: configService.get('db_port'),
+      name: configService.get('db_name'),
+      user: configService.get('db_user'),
+      password: configService.get('db_password'),
+      host: configService.get('db_host'),
+      synchronize: true,
+      autoLoadModels: true,
+      models: []
+    })
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
